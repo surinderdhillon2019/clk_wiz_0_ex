92d8f70164c153bf46ea0de7c5b3399678f2398f
Surinder Pal Singh <surinder312@yahoo.com>
2019-12-08 10:38:39 -0800

    added state machine

--- clk_wiz_0_ex/imports/clk_wiz_0_exdes.v
+++ clk_wiz_0_ex/imports/clk_wiz_0_exdes.v
@@ -8,35 +8,33 @@
 //----------------------------------------------------------------------------
 
 `timescale 1ps/1ps
-
-module clk_wiz_0_exdes 
+module clk_wiz_0_exdes
+#(parameter time PER_DAC_CLK,
+  parameter time PER_PCIE_CLK)
  (
-  output [1:1]   CLK_OUT,
+  output [1:1]  CLK_OUT,
   input         reset,
   input         power_down,
   output        input_clk_stopped,
   output        locked,
- // Clock in ports
-  input         clk_in1
+  input         pcie_clk,
+  input         dac_clk 
  );
 
 
   // Declare the clocks and counter
   wire           clk_int;
-  wire           clk;
+  wire           clk_in1;
 
   // Instantiation of the clocking network
   //--------------------------------------
   clk_wiz_0 clknetwork
    (
-    // Clock out ports
     .clk_out1           (clk_int),
-    // Status and control signals
     .reset              (reset),
     .power_down         (1'b0),
     .input_clk_stopped  (input_clk_stopped),
     .locked             (locked),
-   // Clock in ports
     .clk_in1            (clk_in1)
 );
   ODDRE1 
@@ -47,4 +45,142 @@ module clk_wiz_0_exdes
      .D2 (1'b0),
      .SR (1'b0));
 
+
+/* ################################################## */
+ //-------------Internal Constants--------------------------
+ parameter SIZE = 3 ;
+
+parameter STATE_IDLE = 3'b000 ;
+parameter STATE_DAC_CLK_DETECTED  = 3'b001 ;
+parameter STATE_WAIT_DAC_LOCKED   = 3'b010 ;
+parameter STATE_DAC_CLK_LOCKED    = 3'b011 ;
+parameter STATE_PCIE_CLK_DETECTED = 3'b100 ;
+parameter STATE_WAIT_PCIE_LOCKED  = 3'b101 ;
+parameter STATE_PCIE_CLK_LOCKED   = 3'b110 ;
+
+ //-------------Internal Variables---------------------------
+ reg   [SIZE-1:0]          state        ;// Seq part of the FSM
+ reg   [SIZE-1:0]          next_state   ;// combo part of FSM
+ reg   [15:0]              counter =0   ; // counter to wait for locked signal
+ reg   [15:0]              counter_limit = 100;  // 100 clk cycle -> 16'b‭0000000001100100‬ 
+ reg                       counter_expired = 0 ;
+ reg                       clr_counter     = 1 ;
+ 
+
+ //----------State Machine startes Here------------------------
+ 
+always begin
+  case (state)
+    STATE_IDLE:
+      clk_in1 = #PER_DAC_CLK 1'b0 ;
+    STATE_DAC_CLK_DETECTED:
+      clk_in1 = #PER_DAC_CLK dac_clk ;
+    STATE_WAIT_DAC_LOCKED:
+      clk_in1 = #PER_DAC_CLK dac_clk ;
+    STATE_DAC_CLK_LOCKED:
+      clk_in1 = #PER_DAC_CLK dac_clk ;
+    STATE_PCIE_CLK_DETECTED: 
+      clk_in1 = #PER_PCIE_CLK pcie_clk ;
+    STATE_WAIT_PCIE_LOCKED:
+      clk_in1 = #PER_PCIE_CLK pcie_clk ;
+    STATE_PCIE_CLK_LOCKED:
+      clk_in1 = #PER_PCIE_CLK pcie_clk ;
+    default:
+      clk_in1 = #PER_DAC_CLK 1'b0 ;
+  endcase
+end
+
+
+always @(posedge dac_clk or posedge pcie_clk or posedge reset) begin : STATE_MACHINE
+     // next_state = 3'b000;
+  if (reset) begin 
+    state      = STATE_IDLE;
+    next_state = STATE_IDLE ;
+  end else begin 
+    case (state)
+
+      STATE_IDLE:
+        if ( dac_clk == 1'b1 ) begin           
+          clr_counter <= 1'b0;  // This will trigger the counter 
+          next_state <= STATE_DAC_CLK_DETECTED ;
+        end else if (pcie_clk == 1'b1) begin
+          clr_counter <= 1'b0;
+          next_state  <= STATE_PCIE_CLK_DETECTED ;
+        end
+
+      STATE_DAC_CLK_DETECTED:
+        if ( locked == 1'b1) begin
+          next_state <= STATE_WAIT_DAC_LOCKED;
+        end else if ( counter_expired == 1'b1) begin
+          clr_counter <= 1'b1;  // stop counter
+          next_state <= STATE_IDLE;
+        end
+
+      STATE_WAIT_DAC_LOCKED:
+        if ( locked == 1'b1) begin          
+          next_state <= STATE_DAC_CLK_LOCKED;
+        end
+
+      STATE_DAC_CLK_LOCKED:
+        if ( locked == 1'b0) begin
+          clr_counter <= 1'b1;
+          next_state <= STATE_IDLE;
+        end
+
+      STATE_PCIE_CLK_DETECTED:
+        if ( locked == 1'b1) begin          
+          next_state <= STATE_WAIT_PCIE_LOCKED;
+        end else if ( counter_expired == 1'b1) begin
+          clr_counter <= 1'b1;
+          next_state  <= STATE_IDLE;
+        end
+
+      STATE_WAIT_PCIE_LOCKED:
+        if ( locked == 1'b1) begin
+          next_state <= STATE_PCIE_CLK_LOCKED;
+        end
+
+      STATE_PCIE_CLK_LOCKED:        
+        if ( dac_clk == 1'b1 ) begin
+          clr_counter <= 1'b1;
+          next_state <= STATE_IDLE;
+        end else if (locked == 1'b0) begin 
+          clr_counter <= 1'b1;
+          next_state <= STATE_IDLE;
+        end
+      default : next_state = STATE_IDLE;
+    endcase
+  end
+end
+
+// counter 
+
+always @ ( posedge pcie_clk) begin : COUNTER_SEQ
+  if ( clr_counter == 1'b1) begin  // reset == 1'b1 or
+    counter_expired <= 1'b0;
+    counter         <= 0;
+  end else if (state == STATE_DAC_CLK_DETECTED ) begin
+    counter <= counter + 1'b1;
+    if ( counter == counter_limit) begin 
+      counter_expired <= 1'b1;
+    end
+  end else if (state == STATE_PCIE_CLK_DETECTED) begin 
+    counter <= counter + 1'b1;
+    if ( counter == counter_limit) begin 
+      counter_expired <= 1'b1;
+    end
+  end
+end
+
+//----------Seq Logic-----------------------------
+
+ always @ (posedge pcie_clk) begin : FSM_SEQ
+   if (reset == 1'b1) begin
+     state <= #1  STATE_IDLE;
+   end else begin
+     state <= #1  next_state;
+   end
+ end
+
+/* ################################################## */
 endmodule
--- clk_wiz_0_ex/imports/clk_wiz_0_tb.v
+++ clk_wiz_0_ex/imports/clk_wiz_0_tb.v
@@ -1,6 +1,5 @@
 
 // file: clk_wiz_0_tb.v
-
 //----------------------------------------------------------------------------
 // Clocking wizard demonstration testbench
 //----------------------------------------------------------------------------
@@ -15,34 +14,32 @@
 
 module clk_wiz_0_tb ();
 
-  // timescale is 1ps/1ps
   localparam  ONE_NS      = 1000;
   localparam  PHASE_ERR_MARGIN   = 100; // 100ps
-  // how many cycles to run
-  localparam  COUNT_PHASE = 1024;
+
+  localparam  COUNT_PHASE = 128;
   // we'll be using the period in many locations
-  localparam time PER1    = 3.125*ONE_NS;
+  parameter time PER1    = 3.125*ONE_NS;
   localparam time PER1_1  = PER1/2;
   localparam time PER1_2  = PER1 - PER1/2;
 
-  localparam time PER2    = 3.125*ONE_NS;
+  parameter time PER2    = 3.125*ONE_NS;
   localparam time PER2_1  = PER1/2;
   localparam time PER2_2  = PER1 - PER1/2;
+  reg [7:0]  i = 0;
 
   // Declare the input clock signals
-  reg         clk_in1     = 1;
-  reg         clk_in2     = 1;
-  reg         clk_in1_reset = 0 ;
-  reg         clk_in2_reset = 0 ;
-
+  reg         pcie_clk        = 1'b0;
+  reg         dac_clk         = 1'b0;
+  reg         pcie_clk_reset  = 0;
+  reg         dac_clk_reset   = 0;
+ 
   // Status and control signals
   reg         reset      = 0;
   reg         power_down = 0;
   wire        input_clk_stopped;
   wire        locked;
   wire [1:1]   CLK_OUT;
-
-
 
 //Freq Check using the M & D values setting and actual Frequency generated
   real period1;
@@ -52,21 +49,21 @@ module clk_wiz_0_tb ();
   // Input clock generation
   //------------------------------------
   always begin
-    if (clk_in1_reset == 1'b1) begin 
-      clk_in1 = 1'b0; 
+    if (pcie_clk_reset) begin 
+      pcie_clk = #PER1 1'b0; 
     end else begin
-      clk_in1 = #PER1_1 ~clk_in1;
-      clk_in1 = #PER1_2 ~clk_in1;
+      pcie_clk = #PER1_1 ~pcie_clk;
+      pcie_clk = #PER1_2 ~pcie_clk;
     end
   end
   // Input clock generation
   //------------------------------------
   always begin
-    if (clk_in2_reset == 1'b1) begin 
-      clk_in2 = 1'b0; 
+    if (dac_clk_reset) begin 
+      dac_clk = #PER2 1'b0; 
     end else begin
-      clk_in2 = #PER2_1 ~clk_in2;
-      clk_in2 = #PER2_2 ~clk_in2;
+      dac_clk = #PER2_1 ~dac_clk;
+      dac_clk = #PER2_2 ~dac_clk;
     end
   end
 
@@ -75,29 +72,30 @@ module clk_wiz_0_tb ();
   initial begin
     // Set up any display statements using time to be readable
     $timeformat(-12, 2, "ps", 10);
-    $display ("Timing checks are not valid");
-    COUNTER_RESET = 0;
+    
+    dac_clk_reset = 1;
     test_phase = "reset";
     reset = 1;
     #(PER1*200);
+
     reset = 0;
+    #(PER1*200);
+    dac_clk_reset = 0;    
     test_phase = "wait lock";
-    `wait_lock;
+    // `wait_lock;
     #(PER1*20);
-    COUNTER_RESET = 1;
-    #(PER1*19.2)
-    COUNTER_RESET = 0;
-    #(PER1*0.3)
-    #(PER1*1)
-    $display ("Timing checks are valid");
-    test_phase = "counting";
-    #(PER1*COUNT_PHASE);
+
+    // toggle dac clk on/off 
+    for ( i =0; i < 2 ; i = i +1) begin 
+      // dac_clk_reset = ~dac_clk_reset;
+      #(PER1*COUNT_PHASE);
+    end
+
     if ((period1 -ref_period1_clkin1) <= 100 && (period1 -ref_period1_clkin1) >= -100) begin
-    $display("Freq of CLK_OUT[1] ( in MHz ) : %0f\n", 1000000/period1);
-    end else 
-    begin
-    $display("ERROR: Freq of CLK_OUT[1] is not correct"); 
-    $finish;
+      $display("Freq of CLK_OUT[1] ( in MHz ) : %0f\n", 1000000/period1);
+    end else begin
+      $display("ERROR: Freq of CLK_OUT[1] is not correct"); 
+      $finish;
     end 
     $display("SIMULATION PASSED");
     $display("Test Completed Successfully");
@@ -108,7 +106,10 @@ module clk_wiz_0_tb ();
   // Instantiation of the example design containing the clock
   //    network and sampling counters
   //---------------------------------------------------------
-  clk_wiz_0_exdes 
+
+  clk_wiz_0_exdes
+  #(.PER_DAC_CLK  (PER1) , 
+    .PER_PCIE_CLK (PER2) )
     dut
    (
     .CLK_OUT            (CLK_OUT),
@@ -116,9 +117,10 @@ module clk_wiz_0_tb ();
     .power_down         (power_down),
     .input_clk_stopped  (input_clk_stopped),
     .locked             (locked),
-    .clk_in1            (clk_in1),
-    .clk_in2            (clk_in2)
+    .pcie_clk           (pcie_clk),
+    .dac_clk            (dac_clk)
 );
+
 
 // Freq Check 
 initial
